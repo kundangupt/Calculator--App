58154ba0b3ccba8e4bba4d9b901dac982576f8d3
kundan gupta <kukundan1993@gmail.com>
2023-05-06 14:27:12 +0530

    javascript: updated

---
+++ Calculator.js
@@ -0,0 +1,16 @@
+(function(){
+        let  Screen = document.querySelector('.screen');
+        let  buttons = document.querySelectorAll('.btn');
+        let  Equal = document.querySelector('.btn-equal');
+        let  Clear = document.querySelector('btn-clear');
+    
+
+    buttons.forEach(function (button) {
+        button.addEventListener('click', function(e){
+            let value = e.target.dataset.num;
+             Screen.Value += value;
+    });
+
+});
+
+})();
