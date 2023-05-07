ec53ffca218a5f0c752c0d55a34f5b6aff52348e
kundan gupta <kukundan1993@gmail.com>
2023-05-07 19:40:49 +0530

    style: updated

--- Calculator.js
+++ Calculator.js
@@ -1,38 +1,34 @@
-// function  Calculator (){
-//         let  Screen = document.querySelector('.screen');
-//         let  buttons = document.querySelectorAll('.btn');
-//         let  Equal = document.querySelector('.btn-equal');
-//         let  Clear = document.querySelector('btn-clear');
+function  Calculator (){
+        let  Screen = document.querySelector('.screen');
+        let  buttons = document.querySelectorAll('.btn');
+        let  Equal = document.querySelector('.btn-equal');
+        let  Clear = document.querySelector('btn-clear');
     
 
-//     buttons.forEach(function(button) {
-//         button.addEventListener('click', function(e){
-//             console.log("clcik");
-//             let value = e.target.dataset.num;
-//              Screen.Value += value;
-//     });
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function(e){
+            console.log("clcik");
+            let value = e.target.dataset.num;
+             Screen.value += value;
+    });
 
-// });
+});
 
-// }
-// Calculator();
+     Equal.addEventListener("click", function(e){
+         if(screen.value === ""){
+            screen.value = ""
+         } 
+         else{
+               let answer = eval(screen.value);
+               screen.value = answer;
+         }
+     });
+
+     Clear.addEventListener("click", function(e){
+           screen.value ="";
+     })
+
+}
+Calculator();
 
 
-// function clk(val){
-
-//     document.getElementById("screen").value=document.getElementById("screen").value+val;
-// }
-
-// function clrdisp(){
-//     document.getElementById("screen").value=""
-// }
-
-// function eql(){
-//     var text=document.getElementById("screen").value;
-//     var result=eval(text);
-//     document.getElementById("screen").value=result
-// }
-
-function display(value) {
-        document.getElementById("result").value += value;
-    }
