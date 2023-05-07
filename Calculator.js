47d9b7c091b7ca552a55410f7442a3477b5887be
kundan gupta <kukundan1993@gmail.com>
2023-05-07 19:19:57 +0530

    updated

--- Calculator.js
+++ Calculator.js
@@ -1,17 +1,38 @@
-function  Calculator (){
-        let  Screen = document.querySelector('.screen');
-        let  buttons = document.querySelectorAll('.btn');
-        let  Equal = document.querySelector('.btn-equal');
-        let  Clear = document.querySelector('btn-clear');
+// function  Calculator (){
+//         let  Screen = document.querySelector('.screen');
+//         let  buttons = document.querySelectorAll('.btn');
+//         let  Equal = document.querySelector('.btn-equal');
+//         let  Clear = document.querySelector('btn-clear');
     
 
-    buttons.forEach(function(button) {
-        button.addEventListener('click', function(e){
-            let value = e.target.dataset.num;
-             Screen.Value += value;
-    });
+//     buttons.forEach(function(button) {
+//         button.addEventListener('click', function(e){
+//             console.log("clcik");
+//             let value = e.target.dataset.num;
+//              Screen.Value += value;
+//     });
 
-});
+// });
 
-}
-Calculator();
+// }
+// Calculator();
+
+
+// function clk(val){
+
+//     document.getElementById("screen").value=document.getElementById("screen").value+val;
+// }
+
+// function clrdisp(){
+//     document.getElementById("screen").value=""
+// }
+
+// function eql(){
+//     var text=document.getElementById("screen").value;
+//     var result=eval(text);
+//     document.getElementById("screen").value=result
+// }
+
+function display(value) {
+        document.getElementById("result").value += value;
+    }
