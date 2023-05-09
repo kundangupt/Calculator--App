565e42b2610a91f0193853c00fc5f142d2e4e073
kundan gupta <kukundan1993@gmail.com>
2023-05-09 12:02:26 +0530

    updated

--- Calculator.js
+++ Calculator.js
@@ -1,35 +1,30 @@
-function  Calculator (){
-        let  Screen = document.querySelector('.screen');
-        let  buttons = document.querySelectorAll('.btn');
-        let  Equal = document.querySelector('.btn-equal');
-        let  Clear = document.querySelector('btn-clear');
-    
-
-    buttons.forEach(function(button) {
-        button.addEventListener('click', function(e){
-            console.log("clcik");
-            let value = e.target.dataset.num;
-             Screen.value += value;
+document.addEventListener('DOMContentLoaded', function() {
+    let Screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let Equal = document.querySelector('.btn-equal');
+    let Clear = document.querySelector('.btn-clear');
+  
+    buttons.forEach(function (button) {
+      button.addEventListener('click', function (e) {
+        let value = e.target.dataset.num;
+        Screen.value += value;
+      });
     });
+  
+    Equal.addEventListener("click", function(e){
+        if(Screen.value === "") {
+            Screen.value="please Enter";  
+        } else {
+           let answer = eval(Screen);
+           console.log(answer)
 
-});
+        } 
+      });
 
-     Equal.addEventListener('click', function(e){
-         if(Screen.value === ''){
-            Screen.value = " ";
-         } 
-         else {
-               let answer = eval(Screen.value);
-               Screen.value = answer;
-         }
-     });
-
-     Clear.addEventListener('click', function(){
-             Screen.value =  null;
-           
-     });
-
-}
-Calculator();
+    Clear.addEventListener('click', function (e) {
+       Screen.value = '';       
+    });
+  });
 
 
+ 
