07fe6f2b787f6255b601eb5c62b8b1911de30a68
kundan gupta <kukundan1993@gmail.com>
2023-05-09 14:38:21 +0530

    doc: updated

--- Calculator.js
+++ Calculator.js
@@ -1,30 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     let Screen = document.querySelector('.screen');
     let buttons = document.querySelectorAll('.btn');
-    let Equal = document.querySelector('.btn-equal');
     let Clear = document.querySelector('.btn-clear');
-  
-    buttons.forEach(function (button) {
-      button.addEventListener('click', function (e) {
-        let value = e.target.dataset.num;
-        Screen.value += value;
-      });
-    });
-  
-    Equal.addEventListener("click", function(e){
-        if(Screen.value === "") {
-            Screen.value="please Enter";  
-        } else {
-           let answer = eval(Screen);
-           console.log(answer)
 
-        } 
-      });
+    buttons.forEach(function (button) {
+        button.addEventListener('click', function (e) {
+            let value = e.target.dataset.num;
+            Screen.value += value;
+        });
+    });
 
     Clear.addEventListener('click', function (e) {
-       Screen.value = '';       
+        Screen.value = '';
     });
-  });
+});
+function solve() {
+    let x = document.getElementById("result").value
+    let y = math.evaluate(x);
+    document.getElementById("result").value = y
+}
 
 
- 
